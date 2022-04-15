52c3b27cd5c2f1819501e071ba1adc8b211ecbd7
GPMrks <guilhermepereiramarques@hotmail.com>
2022-04-15 15:13:27 -0300

    DOCS - Added documentation through SpringDoc

---
+++ src/main/java/dio/innovationone/SpringWebMVC/docconfig/SpringDoc.java
@@ -0,0 +1,25 @@
+package dio.innovationone.SpringWebMVC.docconfig;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDoc {
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("REST API Cities para Cadastro de Jedi").version("8.0.0")
+                        .description("Digital Innovation: Expert class - Cadastro de Jedi utilizando API Rest e também Spring MVC")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringDoc for Documentation - springdoc-openapi")
+                        .url("https://springdoc.org/#Introduction"));
+    }
+}
